153dc5996b46413b4fa2567927e6e0033c908187
dembasy <dembasy1227@gmail.com>
2018-07-30 16:18:27 +0200

    done

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+// definition de la fonction truth qui retournera le booléen true
+  function truth(){
+    return true;
+  }
+ ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 1 </title>
+  </head>
+  <body>
+    <p>
+      <?php
+      // affichage de la fonction truth
+      echo truth();
+       ?>
+    </p>
+  </body>
+</html>
